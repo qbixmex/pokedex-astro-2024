7a02ebcf727e2016d1089423f7b8785cbbb97675
Daniel González Briseño <qbixmex@gmail.com>
2024-09-25 16:35:13 -0700

    Main Navigation and Favorites Page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Divider } from "./divider.astro";
 export { default as PokemonCard } from "./pokemons/PokemonCard.astro";
+export { default as Navbar } from "./shared/navbar.astro";
