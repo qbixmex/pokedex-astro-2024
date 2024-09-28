b39ca45a3eae0b050ed771cd928d6e7bdab8e865
Daniel González Briseño <qbixmex@gmail.com>
2024-09-27 20:52:04 -0700

    Astro Islands - (solid js) implementation

---
+++ src/components/Counter.tsx
@@ -0,0 +1,29 @@
+import { createSignal } from "solid-js";
+
+const Counter = () => {
+  const [ counter, setCounter ] = createSignal(10);
+
+  return (
+    <>
+      <h2 class="text-3xl text-orange-600 mb-5">Counter: {counter()}</h2>
+
+      <div class="flex gap-x-2">
+        <button
+          class="bg-blue-600 hover:bg-blue-700 px-3 py-2 rounded transition-colors"
+          onClick={() => setCounter(previousCounterValue => ++previousCounterValue)}
+        >+</button>
+
+        <button
+          class="bg-blue-600 hover:bg-blue-700 px-3 py-2 rounded transition-colors"
+          onClick={() => {
+            if (counter() > 0) {
+              setCounter(previousCounterValue => --previousCounterValue);
+            }
+          }}
+        >-</button>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
